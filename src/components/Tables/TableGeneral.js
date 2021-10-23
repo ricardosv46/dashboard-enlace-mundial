d8281aafab8092ce64e05191df9332470f7fd54f
percy cordova <cordova_inf@hotmail.com>
2021-10-23 12:35:24 -0500

    creando el componente tabla

---
+++ src/components/Tables/TableGeneral.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const TableGeneral = ({
+  dataBody,
+  dataHead,
+  borderColor = 'primary-100',
+  headerTextColor = 'gray-800',
+  bodyTextColor = 'gray-700',
+  headerBgColor = 'white',
+  bodyBGColor = 'white'
+}) => {
+  const handleArray = (object) => {
+    const arreyNew = []
+    for (const property in object) {
+      arreyNew.push(object[property])
+    }
+    return arreyNew
+  }
+  const textColorH = `text-${headerTextColor}`
+  const textColorB = `text-${bodyTextColor}`
+  const bodyBg = `bg-${bodyBGColor}`
+  const headBg = `bg-${headerBgColor}`
+  const borderC = `border-${borderColor}`
+
+  return (
+    <div className="w-full mb-8 overflow-hidden ">
+      <div className="w-full overflow-x-auto">
+        {dataBody && (
+          <table className="w-full  text-center">
+            <thead className="shadow-2xl">
+              <tr
+                className={`text-lg font-bold tracking-wide ${textColorH} 
+              ${headBg} border ${borderC}  text-center`}
+              >
+                {dataHead.map((th, index) => (
+                  <th className={`px-4 py-3 min-w-${th[1]}`} key={index}>
+                    {th[0]}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className={`${bodyBg} border ${borderC} text-base ${textColorB} `}>
+              {dataBody.map((row, index) => (
+                <tr key={index} className=" font-medium">
+                  {handleArray(row).map((column, index) => (
+                    <td key={index}>{column}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default TableGeneral
--- src/views/admin/02-tours/index.js
+++ src/views/admin/02-tours/index.js
@@ -1,10 +1,102 @@
 import React from 'react'
 import Heading from '../../../components/Heading'
+import Input from '../../../components/Input/Input'
+import TableGeneral from '../../../components/Tables/TableGeneral'
 
 const Tours = () => {
+  const dataHead = [['Nombre', 18], ['Edad', 48], ['Domicilio', 48], ['Fecha Ingreso', 52], ['Estado', 48]]
+
+  const dataBody = [
+    {
+      name: 'percy',
+      edad: '27',
+      domicilio: 'AHH NESTOR MARTOS MZ D LT 35',
+      fechaIngreso: '10/01/2020',
+      estado: <button>Pedro</button>
+    },
+    {
+      name: 'percy',
+      edad: '27',
+      domicilio: 'AHH NESTOR MARTOS MZ D LT 35',
+      fechaIngreso: '10/01/2020',
+      estado: <button>Pedro</button>
+    },
+    {
+      name: 'percy',
+      edad: '27',
+      domicilio: 'AHH NESTOR MARTOS MZ D LT 35',
+      fechaIngreso: '10/01/2020',
+      estado: <button>Pedro</button>
+    },
+    {
+      name: 'percy',
+      edad: '27',
+      domicilio: 'AHH NESTOR MARTOS MZ D LT 35',
+      fechaIngreso: '10/01/2020',
+      estado: <button>Pedro</button>
+    },
+    {
+      name: 'percy',
+      edad: '27',
+      domicilio: 'AHH NESTOR MARTOS MZ D LT 35',
+      fechaIngreso: '10/01/2020',
+      estado: <button>Pedro</button>
+    },
+    {
+      name: 'percy',
+      edad: '27',
+      domicilio: 'AHH NESTOR MARTOS MZ D LT 35',
+      fechaIngreso: '10/01/2020',
+      estado: <button>Pedro</button>
+    },
+    {
+      name: 'percy',
+      edad: '27',
+      domicilio: 'AHH NESTOR MARTOS MZ D LT 35',
+      fechaIngreso: '10/01/2020',
+      estado: <button>Pedro</button>
+    },
+    {
+      name: 'percy',
+      edad: '27',
+      domicilio: 'AHH NESTOR MARTOS MZ D LT 35',
+      fechaIngreso: '10/01/2020',
+      estado: <button>Pedro</button>
+    },
+    {
+      name: 'percy',
+      edad: '27',
+      domicilio: 'AHH NESTOR MARTOS MZ D LT 35',
+      fechaIngreso: '10/01/2020',
+      estado: <button>Pedro</button>
+    },
+    {
+      name: 'percy',
+      edad: '27',
+      domicilio: 'AHH NESTOR MARTOS MZ D LT 35',
+      fechaIngreso: '10/01/2020',
+      estado: <button>Pedro</button>
+    },
+    {
+      name: 'percy',
+      edad: '27',
+      domicilio: 'AHH NESTOR MARTOS MZ D LT 35',
+      fechaIngreso: '10/01/2020',
+      estado: <button>Pedro</button>
+    },
+    {
+      name: 'percy',
+      edad: '27',
+      domicilio: 'AHH NESTOR MARTOS MZ D LT 35',
+      fechaIngreso: '10/01/2020',
+      estado: <button>Pedro</button>
+    }
+  ]
   return (
     <div className="shadow md:rounded bg-white p-5 py-10 md:p-10">
       <Heading>Tours</Heading>
+      <Input />
+      <TableGeneral dataBody={dataBody} dataHead={dataHead} />
     </div>
   )
 }
