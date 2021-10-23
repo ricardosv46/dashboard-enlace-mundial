ecaa8a758cc7e2cd204fc37d23eda6ac2d40a020
Diego Pacheco <ericksonder017@gmail.com>
2021-10-23 15:20:26 -0500

    Agregando el formulario crearTour

--- src/views/admin/02-tours/index.js
+++ src/views/admin/02-tours/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import Heading from '../../../components/Heading'
-import CrearTour from './CrearTour'
 
 const Tours = () => {
   return (
     <div className="shadow md:rounded bg-white p-5 py-10 md:p-10">
       <Heading>Tours</Heading>
-      <div>
-        <CrearTour />
-      </div>
+      <div></div>
     </div>
   )
 }
