2b3750c82241427e7dee6aa4096e737557bd760b
Diego <ericksonder017@gmail.com>
2021-10-25 10:41:22 -0500

    Corrigiendo bug del InputToggle

--- src/components/Forms/InputToggle/InputToggle.js
+++ src/components/Forms/InputToggle/InputToggle.js
@@ -1,14 +1,33 @@
+import { useState } from 'react'
 
-const InputToggle = ({ Toggle, SetToggle }) => {
-  const Cambio = () => {
-    SetToggle(!Toggle)
-  }
+const InputToggle = ({ status = true }) => {
+  const [state, setState] = useState(status)
+
+  const handleChange = () => setState((prev) => !prev)
+  console.log(handleChange)
+
   return (
-    <label htmlFor="toggle" className="flex items-center cursor-pointer justify-center">
+    <label
+      // htmlFor="toggle"
+      className="flex items-center cursor-pointer justify-center"
+    >
       <div className="relative">
-        <input type="checkbox" id="toggle" className="sr-only" onClick={Cambio} />
-        <div className={`block ${Toggle ? 'bg-primary' : 'bg-accent'}  w-14 h-7 rounded-full`}></div>
-        <div className={` absolute left-1 top-1  w-5 h-5 rounded-full transition ${Toggle ? ' transform translate-x-full bg-white' : ' bg-primary'} `}></div>
+        <input
+          type="checkbox"
+          // id="toggle"
+          className="sr-only"
+          onClick={handleChange}
+        />
+        <div
+          className={`block ${
+            state ? 'bg-primary' : 'bg-accent'
+          }  w-14 h-7 rounded-full`}
+        ></div>
+        <div
+          className={` absolute left-1 top-1  w-5 h-5 rounded-full transition ${
+            state ? ' transform translate-x-full bg-white' : ' bg-primary'
+          } `}
+        ></div>
       </div>
     </label>
   )
