11b51dadf43c081206847eab25e916b0bb14d07d
Diego <ericksonder017@gmail.com>
2021-10-25 10:07:19 -0500

    Agregando animacion al sidebar

--- src/components/Sidebar/SidebarLink.js
+++ src/components/Sidebar/SidebarLink.js
@@ -4,16 +4,19 @@ import { NavLink } from 'react-router-dom'
 const SidebarLink = ({ icon: Icon, label, to = '/' }) => {
   return (
     <NavLink
-    activeClassName="bg-primary-700 text-gray-200 text-lg"
-      exact to={to}
-      className="relative flex flex-row items-center text-md h-11 focus:outline-none hover:bg-primary-600 text-gray-300 hover:text-gray-200 border-l-4 border-transparent hover:border-blue-500 pr-6 transition-colors"
+      activeClassName="bg-primary-700 text-gray-200 text-lg"
+      exact
+      to={to}
+      className="flex focus:outline-none text-md hover:bg-primary-600 text-gray-300 hover:text-gray-200 border-l-4 border-transparent hover:border-blue-500 transition-colors"
     >
-      <span className="inline-flex justify-center items-center ml-4">
-        {Icon ? <Icon className="w-7 h-5" /> : null}
-      </span>
-      <span className="ml-2 font-medium tracking-wide truncate font-sans">
-        {label}
-      </span>
+      <p className="flex items-center h-11 w-full transform hover:translate-x-2 duration-300">
+        <span className="inline-flex justify-center items-center ml-4">
+          {Icon ? <Icon className="w-7 h-5" /> : null}
+        </span>
+        <span className="ml-2 font-medium tracking-wide truncate font-sans">
+          {label}
+        </span>
+      </p>
     </NavLink>
   )
 }
