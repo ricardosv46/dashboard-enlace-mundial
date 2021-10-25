988001c22074a10c1a580c728671ec658a60b9f4
Diego <ericksonder017@gmail.com>
2021-10-25 12:46:24 -0500

    Agregando funcionalidad al boton BtnDestacado

--- src/components/BtnDestacado/BtnDestacado.js
+++ src/components/BtnDestacado/BtnDestacado.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IconHeartEmpty, IconHeartSolid } from '../../assets/icons/icons'
 // import destacoFalse from '../../assets/imgs/icon_corazon_false.svg'
 // import destacoTrue from '../../assets/imgs/icon_corazon_true.svg'
-const BtnDestacado = ({ estado = false }) => {
+const BtnDestacado = ({ estado = false, disabled = false }) => {
+  const [state, setstate] = useState(estado)
+
+  const toggle = () => setstate((prev) => !prev)
+
   return (
     // <>
     //   <img src={estado ? destacoTrue : destacoFalse} className="w-6" />
     // </>
-    estado ? <IconHeartSolid /> : <IconHeartEmpty />
+    <button disabled={disabled} onClick={toggle}>
+      {state ? <IconHeartSolid /> : <IconHeartEmpty />}
+    </button>
   )
 }
 
