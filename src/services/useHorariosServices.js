7bcf8564db72e1b0fb4f90cd444dd568df491f19
Diego <ericksonder017@gmail.com>
2021-11-13 18:42:42 -0500

    Agregando funcionalidad para crear horarios para los tours

--- src/services/useHorariosServices.js
+++ src/services/useHorariosServices.js
@@ -1,7 +1,14 @@
-import { useGetHorariosTourQuery } from '../generated/graphql'
+import { useState } from 'react'
+import swal from 'sweetalert'
+import {
+  useCreateHorarioTourMutation,
+  useGetHorariosTourQuery
+} from '../generated/graphql'
 
 export const useHorariosServices = (id) => {
-  const { loading, data: dataHorarios } = useGetHorariosTourQuery({
+  const [loading, setLoading] = useState(false)
+
+  const { loading: loadingGet, data: dataHorarios } = useGetHorariosTourQuery({
     fetchPolicy: 'network-only',
     variables: {
       tourId: id,
@@ -12,5 +19,49 @@ export const useHorariosServices = (id) => {
 
   const data = dataHorarios ? dataHorarios?.GetHorariosTour : []
 
-  return { data, loading }
+  // MUTATION PARA CREAR HORARIO
+  const [createHorarioMutation, { loading: loadingCreate }] =
+    useCreateHorarioTourMutation({
+      onError: (error) => {
+        // validar errores
+        console.log('onError creacion horario ', error)
+      }
+    })
+  // FUNCION QUE CONSUME EL MUTATION PARA CREAR HORARIO
+  const createHorario = async (horario) => {
+    const response = await createHorarioMutation({
+      variables: {
+        input: {
+          hora: horario.hora,
+          cupos: horario.cupos,
+          precio: horario.precio,
+          tourId: horario.tourId,
+          fecha: horario.fecha,
+          estado: horario.estado
+        }
+      }
+    })
+
+    if (response?.data?.CreateHorarioTour) {
+      swal({
+        title: 'Horario creado',
+        icon: 'success',
+        timer: 1200
+      })
+      console.log('horario creado ', response.data?.CreateHorarioTour)
+      return 'ok'
+    }
+  }
+
+  if (loadingGet === true || loadingCreate === true) {
+    if (loading === false) {
+      setLoading(true)
+    }
+  } else {
+    if (loading === true) {
+      setLoading(false)
+    }
+  }
+
+  return { data, loading, createHorario }
 }
